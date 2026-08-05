4ce13f8150633e755824ea4aa1ae7ce6e56b8d75
HarshitJain-hbtu <harshit1811fzd@gmail.com>
2026-08-06 00:06:41 +0530

    32 ms (61.17%) 208.5 MB (17.07%) 08-06-2026 Medium .java Depth-First Search

---
+++ LeetCode/Medium/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph-08-06-2026-00-06-40.java
@@ -0,0 +1,44 @@
+class Solution {
+    int n ;
+    public long countPairs(int N, int[][] edges) {
+        // M-2 using DFS | time O(n + m) | space O(n+ m) // m = edges.length
+        // here things divided into components like that and then calculate size of components using dfs and bfs
+        // using formula to count pairs:  size * (remain - size)  and update remain 
+        n = N;
+        // creating graph 
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i< n;i++){
+            adj.add( new ArrayList<Integer>());
+        }
+        for (int [] arr : edges){
+            int u = arr[0];
+            int v = arr[1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        boolean [] visited = new boolean[n];
+        long result = 0;
+        long remainNodes = n;
+        for (int i = 0 ;i < n;i++){
+            if (! visited[i]){
+                int size = DFS(adj , i , visited);
+                result += size * (remainNodes - size);
+                remainNodes -= size;
+            }
+        }
+        return result;
+    }
+    private int DFS (ArrayList<ArrayList<Integer>> adj , int u , boolean[] visited ){
+        visited[u] = true; 
+        int size = 1;
+
+        for (int v  : adj.get(u)){
+            if (! visited[v]){
+                size += DFS(adj , v , visited);
+            }
+        }
+        return size ;
+    }
+}
