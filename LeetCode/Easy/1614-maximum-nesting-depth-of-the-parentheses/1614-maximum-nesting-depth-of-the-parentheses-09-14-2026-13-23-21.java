0375f4e64087f1b618f9a037b980cdad055fdaf8
Harshit Jain <harshit1811fzd@gmail.com>
2026-09-14 13:23:23 +0530

    0 ms (100%) 42.7 MB (70.22%) 09-14-2026 Easy .java String

---
+++ LeetCode/Easy/1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses-09-14-2026-13-23-21.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        // M-2 optimal approach  | time O(n) | space O(n)
+        // use count var to just count nested one , when we get '(' we count++ and update depth and when ')' then count -- , hence count store no of nested ( 
+        int depth = 0 ;
+        int count = 0 ;
+        for (char ch : s.toCharArray()){
+            if (ch == '('){
+                count++;
+                depth = Math.max(depth , count);
+            }
+            if (ch == ')'){
+                count-- ;
+            }
+        }
+        return depth ;
+    }
+}
