8caae647a6a25b576832f9e9faa2b18b3ac9d477
Harshit Jain <harshit1811fzd@gmail.com>
2026-09-17 17:30:05 +0530

    3 ms (94%) 44.1 MB (94.84%) 09-17-2026 Easy .java Hash Table

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram-09-17-2026-17-30-04.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // M -3 optimal approach | time O(n) | space O(1)
+        if (s.length() != t.length()){
+            return false;
+        }
+        int n = s.length();
+        int [] nums = new int[26];
+        for (int i = 0 ; i < n ;i++){
+            nums[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0 ; i < n ;i++){
+            if (nums[t.charAt(i) - 'a'] == 0){
+                return false;
+            }
+            else{
+                nums[t.charAt(i) - 'a']--;
+            }
+        }
+        return true;
+    }
+}
